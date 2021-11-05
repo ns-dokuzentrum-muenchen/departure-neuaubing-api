da88391621c4437942120179f092c6df8a398d50
N Kort <studio@n-kort.net>
2021-11-05 17:32:47 +0100

    isset() there too

--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ add_filter('wp_insert_post_data', 'default_comments_on');
 // add_filter('rest_allow_anonymous_comments', '__return_true');
 
 add_filter('user_has_cap', function ($all, $cap, $args, $user) {
-  if (is_user_logged_in() && $cap[0] == 'edit_posts') {
+  if (is_user_logged_in() && isset($cap[0]) && $cap[0] == 'edit_posts') {
     if ($_SERVER['REQUEST_URI'] == '/wp-json/wp/v2/markierung') {
       if (isset($user->caps['subscriber']) && $user->caps['subscriber']) {
         $all['edit_posts'] = 1;
