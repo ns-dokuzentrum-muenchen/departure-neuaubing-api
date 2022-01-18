1702ca7f39e74ed6cae96a3a3bfa4c9753b46852
N Kort <studio@n-kort.net>
2022-01-18 12:31:23 +0000

    fix return_to url

--- custom-endpoints.php
+++ custom-endpoints.php
@@ -67,7 +67,7 @@ function dn_search (WP_REST_Request $request) {
 function passwordless_login (WP_REST_Request $request) {
   $account = ( isset( $request['user_email_username']) ) ? $account = sanitize_text_field( $request['user_email_username'] ) : false;
   $nonce = ( isset( $_POST['nonce']) ) ? $nonce = sanitize_key( $_POST['nonce'] ) : false;
-  $return_to = ( isset( $_POST['return_to']) ) ? $return_to = esc_url_raw( $_POST['return_to'] ) : false;
+  $return_to = ( isset( $_POST['return_to']) ) ? $return_to = sanitize_text_field( rawurldecode( $_POST['return_to'] ) ) : false;
 
   $msg = '';
   $status = 200;
@@ -77,7 +77,7 @@ function passwordless_login (WP_REST_Request $request) {
     $status = 400;
   } else {
     if (function_exists('wpa_send_link')) {
-      $sent_link = wpa_send_link($account, $nonce, $return_to);
+      $sent_link = wpa_send_link($account, $nonce, urlencode($return_to));
     } else {
       $msg = '404 Not found';
       $status = 404;
@@ -110,7 +110,7 @@ function dn_register (WP_REST_Request $request) {
   $username = sanitize_text_field( $body['username'] );
   $email = sanitize_email( $body['email'] );
   $nonce = sanitize_key( $body['nonce'] );
-  $return_to = esc_url_raw( $body['return_to'] );
+  $return_to = sanitize_text_field( rawurldecode( $body['return_to'] ) );
 
   $response = new WP_REST_Response();
 
@@ -130,7 +130,7 @@ function dn_register (WP_REST_Request $request) {
       return $response;
     } else {
       // user has been created, send a login link
-      $sent_link = wpa_send_link($username, $nonce, $return_to);
+      $sent_link = wpa_send_link($username, $nonce, urlencode($return_to));
 
       if (!is_wp_error($sent_link)) {
         $msg = apply_filters('wpa_success_link_msg', __('Please check your email. You will soon receive an email with a login link.', 'passwordless-login'));
@@ -226,25 +226,6 @@ function dn_search_places (WP_REST_Request $request) {
     $response = new WP_REST_Response($res);
     $response->set_status(200);
     return $response;
-    // foreach ($query->posts as $post) {
-    //   $acf = get_fields($post->ID);
-
-    //   $post->id = intval($post->ID);
-    //   $post->categories = wp_get_post_categories($post->ID, array('fields' => 'all'));
-    //   $post->tags = wp_get_post_tags($post->ID);
-    //   $post->acf = $acf;
-    //   $post->title_highlighted = relevanssi_highlight_terms(html_entity_decode($post->post_title), $keyword);
-    //   $post->link = get_the_permalink($post->ID);
-    //   $post->slug = $post->post_name;
-
-    //   if ($acf) {
-    //     $text = $acf['biographie'] ?? $acf['description']; // add more?
-    //     $post->content_highlighted = relevanssi_highlight_terms($text, $keyword, true);
-    //   }
-    // }
-    // $response = json_encode($query->posts);
-    // $response = json_decode($response, true);
-    // return new WP_REST_RESPONSE($response, 200);
   } else {
     return new WP_Error('no_posts', 'Nothing found', array('status' => 404));
   }
