2a394ce48c28503fcd012b7f97de791bbcfb746b
N Kort <studio@n-kort.net>
2022-01-22 15:03:59 +0000

    redirect from api theme index.php

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
   // required for login flow
   header('X-WP-Nonce: ' . wp_create_nonce('wpa_passwordless_login_request'));
 ?>
+<!doctype html>
+<html>
+  <head>
+    <title>Redirecting...</title>
+    <meta http-equiv="refresh" content="3; URL=https://departure-neuaubing.nsdoku.de"/>
+    <style>
+      body { margin: 2em; font-family: monospace; }
+      h1 { font-size: 1em; }
+    </style>
+  </head>
+  <body>
+    <h1>departure neuaubing, ns-dokumentationszentrum münchen</h1>
 
-<pre>
-
-    departure neuaubing, ns-dokumentationszentrum münchen
-
-    coming soon
-
-</pre>
+    <p>Redirecting to <a href="https://departure-neuaubing.nsdoku.de">departure-neuaubing.nsdoku.de</a></p>
+  </body>
+</html>
